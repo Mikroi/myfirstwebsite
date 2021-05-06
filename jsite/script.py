611acd833e68a965a9bf2fc90582db38bb79fb6d
jimmy <jimmy.snowden@gmail.com>
2021-05-06 18:18:35 -0400

    whatever you want

--- jsite/script.py
+++ jsite/script.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+# home page
+
 @app.route('/')
 def home():
     return render_template("home.html")
+
+# about page
 
 @app.route('/about/')
 def about():
